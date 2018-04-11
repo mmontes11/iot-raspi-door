108b085bd8af06535e73dd2db63393c669d5900d
mmontes11 <martin11lrx@gmail.com>
2018-04-11 20:09:51 +0000

    Improved examples

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import log from './utils/log';
 import onoff from 'onoff';
 
-const gpio = new onoff.Gpio(17, "in", "rising", {debounceTimeout: 10});
+const gpio = new onoff.Gpio(4, "in", "rising", {debounceTimeout: 10});
 
 gpio.watch((err, value) => {
-    if (err) {
-	console.log(err);
-        throw err;
-    } else {
-        log.logInfo(value)
-    }
+	if (err) {
+		throw err;
+	} else {
+		log.logInfo(value)
+	}
 });;
