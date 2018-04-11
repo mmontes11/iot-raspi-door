49195804009d60669c17512cf14c932f7a9bef15
mmontes11 <martin11lrx@gmail.com>
2018-04-11 20:24:41 +0000

    Polling GPIO every 100 ms

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import log from './utils/log';
 import onoff from 'onoff';
 
-const gpio = new onoff.Gpio(4, "in", "rising", {debounceTimeout: 10});
+const gpio = new onoff.Gpio(4, "in");
 
-gpio.watch((err, value) => {
-	if (err) {
-		throw err;
-	} else {
-		log.logInfo(value)
-	}
-});;
+setInterval(() => {
+	log.logInfo(gpio.readSync());
+}, 100);
