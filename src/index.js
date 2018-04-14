07949de5e571696cef7ecac4d2cfec75d8972ebe
mmontes11 <martin11lrx@gmail.com>
2018-04-14 17:21:34 +0000

    Added TODOs

--- src/index.js
+++ src/index.js
@@ -8,7 +8,25 @@ const ldr = new LDR(config.ldrGpio, config.ldrDarkRcTimeThreeshold);
 
 doorSensor.onChanged((isOpened) => {
 	log.logInfo(`Door sensor: ${isOpened ? "opened" : "closed"}`);
-	log.logInfo(`LDR: ${ldr.isDark() ? "dark" : "light"}`);
+	const isDark = ldr.isDark();
+	log.logInfo(`LDR: ${isDark ? "dark" : "light"}`);
+
+	if (isOpened && isDark) {
+		//TODO: Turn the light bulb on
+        log.logInfo(`Light bulb: on`);
+    } else {
+        //TODO: Turn the light bulb off
+        log.logInfo(`Light bulb: off`);
+    }
+    if (isOpened) {
+		//TODO: Turn opened LED on
+        //TODO: Turn closed LED off
+        //TODO: Send door-opened event
+	} else {
+        //TODO: Turn opened LED off
+        //TODO: Turn closed LED on
+        //TODO: Send door-closed event
+    }
 });
 
 process.on('SIGINT', () => {
