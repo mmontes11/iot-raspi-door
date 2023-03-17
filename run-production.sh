2e622a292ff455161c12b2a733ed9287c3d0362a
Martin Montes <martin11lrx@gmail.com>
2023-03-17 22:32:06 +0100

    Migrated to GHCR

--- run-production.sh
+++ run-production.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --name iot-raspi-door --privileged --network host --restart always --env-file .env -h $(hostname) -d mmontes11/iot-raspi-door
+docker run --name iot-raspi-door --privileged --network host --restart always --env-file .env -h $(hostname) -d ghcr.io/mmontes11/iot-raspi-door
